63a1ca91d2d9e9e93372dc91932cf70766c5992a
Eecodev <mnasser.bbk@gmail.com>
2024-01-23 15:52:48 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Ciao gaters</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeView',
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
