931fc5e2cd0f46c706ca649ef25774f69c5ee1ed
matteobonelli <matteobonelli08@gmail.com>
2024-01-26 15:57:34 +0100

    character view

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -1,35 +1,36 @@
 <template>
-     <main id="characters-show">
-    <section class="container" id="characters_index">
-      <div class="row py-5">
-        <div class="col-4">
-          <h1 v-if="character" class="text-center display-3">{{ character.name }}</h1>
-          <img :src="store.imgUrl + character.image" class="w-100" :alt="character.name">
-        </div>
-        <div class="col d-flex flex-column justify-content-center">
-          <p class="text-light fs-3 mx-5">description: <br> {{ character.description }}</p>
+    <main id="characters-show">
+        <section class="container" id="characters_index">
+            <div class="row py-5">
+                <div class="col-4">
+                    <h1 v-if="character" class="text-center display-3">{{ character.name }}</h1>
+                    <img :src="store.imgUrl + character.image" class="w-100" :alt="character.name">
+                </div>
+                <div class="col d-flex flex-column justify-content-center">
+                    <p class="text-light fs-3 mx-5">description: <br> {{ character.description }}</p>
 
-          <div class="d-flex fs-5 w-100 mx-5">
-            <span>Type: {{ character.type ? character.type.name : 'Unknown' }}</span>
-            <span>Attack: {{ character.attack }}</span>
-            <span>Defence: {{ character.defence }}</span>
-            <span>Speed: {{ character.speed }}</span>
-            <span>Life: {{ character.life }}</span>
-          </div>
+                    <div class="d-flex fs-5 w-100 mx-5">
+                        <span>Type: {{ character.type ? character.type.name : 'Unknown' }}</span>
+                        <span>Attack: {{ character.attack }}</span>
+                        <span>Defence: {{ character.defence }}</span>
+                        <span>Speed: {{ character.speed }}</span>
+                        <span>Life: {{ character.life }}</span>
+                    </div>
 
-          <div class="d-flex mx-5 mt-3">
-            <div v-for="item in character.items" :key="item.id" class="badge rounded-pill items-color me-2">{{ item.name }}</div>
-          </div>
+                    <div class="d-flex mx-5 mt-3">
+                        <div v-for="item in character.items" :key="item.id" class="badge rounded-pill items-color me-2">{{
+                            item.name }}</div>
+                    </div>
 
-          <!-- <div class="d-flex mx-5">
+                    <!-- <div class="d-flex mx-5">
             <router-link :to="{ name: 'characters-edit', params: { id: character.id } }" class="btn btn-light fs-5 my-3">modifica</router-link>
 
             <button @click="deleteCharacter" class="cancel-button btn btn-danger fs-5 my-3 mx-2 py-2 px-3" :data-item-title="character.name">cancella</button>
           </div> -->
-        </div>
-      </div>
-    </section>
-  </main>
+                </div>
+            </div>
+        </section>
+    </main>
     <!-- <h1 v-if="character">{{ character.name }}</h1>
     <img :src="store.imgUrl + character.image" :alt="character.name"> -->
 </template>
@@ -67,7 +68,7 @@ export default {
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
-    height:calc(100vh - 56px);
+    height: calc(100vh - 56px);
     width: 100%;
     display: flex;
     justify-content: center;
